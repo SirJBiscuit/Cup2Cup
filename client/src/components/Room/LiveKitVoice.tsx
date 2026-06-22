80908710c734b4879a738333288e4689f0b6cb43
SirJBiscuit <jcompute256@gmail.com>
2026-06-21 22:56:21 -0400

    Use Twilio TURN servers with multiple transports for better NAT traversal

--- client/src/components/Room/LiveKitVoice.tsx
+++ client/src/components/Room/LiveKitVoice.tsx
@@ -89,14 +89,22 @@ const LiveKitVoice = ({ roomName, displayName, onReady, onError }: LiveKitVoiceP
           rtcConfig: {
             iceServers: [
               {
-                urls: 'stun:stun.l.google.com:19302',
+                urls: [
+                  'stun:stun.l.google.com:19302',
+                  'stun:global.stun.twilio.com:3478',
+                ],
               },
               {
-                urls: 'turn:openrelay.metered.ca:80',
-                username: 'openrelayproject',
-                credential: 'openrelayproject',
+                urls: [
+                  'turn:global.turn.twilio.com:3478?transport=udp',
+                  'turn:global.turn.twilio.com:3478?transport=tcp',
+                  'turn:global.turn.twilio.com:443?transport=tcp',
+                ],
+                username: 'f4b4035eaa76f4a55de5f4351567653ee4ff6fa97b50b6b334fcc1be9c27212d',
+                credential: 'w1uxM55V9yVoqyVFjt+mxDBV0F87AUCemaYVQGxsPLw=',
               },
             ],
+            iceTransportPolicy: 'all',
           },
         });
 
